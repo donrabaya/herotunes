8a3768b7cc87e833765531f785a9fde1fca384fa
Fedricco <fedriccorabaya@gmail.com>
2018-09-20 13:01:50 +0800

    Updated Herotunes

--- album.php
+++ album.php
@@ -6,19 +6,26 @@ if(isset($_GET['id'])) {
 	header("Location: index.php");
 }
 
-$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id = '$albumId'");
-$album = mysqli_fetch_array($albumQuery);
+$album = new Album($con, $albumId);
 
-$artistId = $album['artist'];
-
-$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id ='$artistId'");
-$artist = mysqli_fetch_array($artistQuery);
-
-echo $album['title'] . "<br>";
-echo $artist['name'];
+$artist = $album->getArtist();
 
 ?>
 
+<div class="entityInfo">
+	
+	<div class="leftSection">
+		<img src="<?php echo $album->getArtworkPath(); ?>">
+	</div>
+
+	<div class="rightSection">
+		<h2><?php echo $album->getTitle(); ?></h2>
+		<p>By <?php echo $artist->getName(); ?></p>
+		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+
+	</div>
+
+</div>
 
 
 <?php include('includes/footer.php'); ?>
